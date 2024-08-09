f440ae1cbcc251da327b581f1fd5b524e66ec344
vilarj <lamaravillasan@outlook.com>
2024-08-09 17:34:13 -0400

    Adding new favicon | Adding assets | renaming the root tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'DomiScan';
