0470a54305ca82ec7a9caa0456817af0fc3b8087
vilarj <lamaravillasan@outlook.com>
2024-08-10 12:22:44 -0400

    Firebase up and running | Using Reactive Forms for the scores

--- src/app/box-score/box-score.component.ts
+++ src/app/box-score/box-score.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'Box-Score',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './box-score.component.html',
   styleUrl: './box-score.component.css',
 })
-export class BoxScoreComponent {}
+export class BoxScoreComponent {
+  boxForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.boxForm = this.formBuilder.group({
+      player1: [],
+      player2: [],
+    });
+  }
+}
